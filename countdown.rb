72352abd9a41ebb4d6ee45a488be750e4b19b734
Noah Alston <crashlive112@gmail.com>
2020-09-13 19:40:16 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,8 +3,8 @@
 def countdown(number)
   while number >= 0 do
     puts "#{number} SECOND(S)!"
-    number -= 1
-    puts "HAPPY NEW YEAR!"
+    number -= 1  
   end
-
 end
+
+puts "HAPPY NEW YEAR!"
